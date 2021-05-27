d6c79622b23f7cd5ff1906fb96ee837914830913
juno-choi <ililil9482@naver.com>
2021-05-27 21:57:52 +0900

    추가 : security 설정과 jwt 설정

--- src/main/java/com/juno/gakebu/api/domain/member/Member.java
+++ src/main/java/com/juno/gakebu/api/domain/member/Member.java
@@ -1,28 +1,78 @@
 package com.juno.gakebu.api.domain.member;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     @GeneratedValue
-    private Long id;
+    private Long idx;
     @Column(nullable = false, unique = true, length = 20)
     private String memberId;
     @Column(nullable = false, unique = true, length = 30)
     private String name;
+    @Column(nullable = true, length = 30)
+    private String pw;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.pw;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Override
+    public String getUsername() {
+        return this.memberId;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
