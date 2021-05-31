267c56accbf559595b6da1185dd39ca9df52c7a8
juno-choi <ililil9482@naver.com>
2021-05-31 22:33:25 +0900

    MockMvc를 이용한 테스트 코드 작성

--- src/test/java/com/juno/gakebu/api/service/member/MemberServiceTest.java
+++ src/test/java/com/juno/gakebu/api/service/member/MemberServiceTest.java
@@ -1,20 +1,46 @@
 package com.juno.gakebu.api.service.member;
 
-import com.juno.gakebu.api.domain.member.Member;
-import com.juno.gakebu.api.repository.member.MemberRepository;
-import org.assertj.core.api.Assertions;
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @Transactional
 class MemberServiceTest {
 
-    @Autowired MemberService memberService;
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    private MockMvc mock;
+
+    @Test
+    public void 회원원가입_테스트() throws Exception{
+        JsonObject json = new JsonObject();
+        json.addProperty("memberId", "test");
+        json.addProperty("pw", "1234");
+        json.addProperty("email", "email");
+
+        System.out.println(json.toString());
+        mock.perform(post("/v1/join").contentType(MediaType.APPLICATION_JSON).content(json.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.code").value(0));
+    }
+
 
 }
